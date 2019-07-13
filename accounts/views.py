e2b943abec73f465f2f11b775a53108e95187169
alexHooper <alexmohack@gmail.com>
2019-07-13 08:15:43 +0530

    define default activate view

--- accounts/views.py
+++ accounts/views.py
@@ -1,3 +1,26 @@
 from django.shortcuts import render
+from django.contrib.auth import get_user_model
+from django.utils.http import urlsafe_base64_decode, is_safe_url
+from django.views import View
 
-# Create your views here.
+from .tokens import account_activation_token
+
+User = get_user_model()
+
+class AccountActivate(View):
+	def get(self, request, uidb64, token):
+		try:
+			uid = urlsafe_base64_decode(uidb64).decode()
+			user = User.objects.get(pk=uid)
+		except (TypeError, ValueError, OverflowError, User.DoesNotExist) as e:
+			print(e)
+			user = None
+
+		if user is not None and account_activation_token.check_token(user, token):
+			user.is_active = True
+			user.save()
+			# login(request, user, backend='django.contrib.auth.backends.ModelBackend')
+			# return redirect('login')
+			return render(request, 'registration/account_activated.html')
+		else:
+			return render(request, 'registration/account_activation_invalid.html')
