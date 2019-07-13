a24d0770ec859b6407f32753fa4b9b65da06406b
alexHooper <alexmohack@gmail.com>
2019-07-13 07:48:39 +0530

    use jwt to encode token

--- accounts/signals.py
+++ accounts/signals.py
@@ -1,20 +1,27 @@
+import jwt
+
 from django.dispatch import receiver
 from django.core.mail import EmailMessage
 from django.db.models.signals import pre_save, post_save
 from django.contrib.auth import get_user_model
 from django.template.loader import render_to_string
 
+from .tokens import account_activation_token
+
 User = get_user_model()
 
 @receiver(post_save, sender=User)
 def post_save_user(sender, instance, created, *args, **kwargs):
 	if created:
-		message = render_to_string('registration/emails/account_activation_email.html', {
+		data = {
 			'user': user,
-			'domain': domain,
 			# 'uidb64': urlsafe_base64_encode(force_bytes(user.pk)).decode(),
 			'uidb64': urlsafe_base64_encode(force_bytes(user.pk)),
 			'token': account_activation_token.make_token(user)
+		}
+		key = jwt.encode(data, settings.SECRET_KEY, algorithm='HS256')
+		message = render_to_string('registration/emails/account_activation_email.html', {
+			'key': key
 		})
 
 		# you can also use 'email_user' method from custom User model
